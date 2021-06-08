dee2a37da86c0da2a5e997732317c7704b109296
Michael Matloka <dev@twixes.com>
2021-06-08 12:03:53 +0200

    Add prettier

--- cli.js
+++ cli.js
@@ -5,25 +5,25 @@ const program = require('commander')
 const PostHog = require('.')
 const pkg = require('./package')
 
-const toObject = str => JSON.parse(str)
+const toObject = (str) => JSON.parse(str)
 
 program
-  .version(pkg.version)
-  .option('-k, --apiKey <key>', 'the PostHog api key to use')
-  .option('-h, --host <host>', 'the PostHog API hostname to use')
-  .option('-t, --type <type>', 'the PostHog message type')
+    .version(pkg.version)
+    .option('-k, --apiKey <key>', 'the PostHog api key to use')
+    .option('-h, --host <host>', 'the PostHog API hostname to use')
+    .option('-t, --type <type>', 'the PostHog message type')
 
-  .option('-d, --distinctId <id>', 'the distinct id to send the event as')
+    .option('-d, --distinctId <id>', 'the distinct id to send the event as')
 
-  .option('-e, --event <event>', 'the event name to send with the event')
-  .option('-p, --properties <properties>', 'the event properties to send (JSON-encoded)', toObject)
+    .option('-e, --event <event>', 'the event name to send with the event')
+    .option('-p, --properties <properties>', 'the event properties to send (JSON-encoded)', toObject)
 
-  .option('-a, --alias <previousId>', 'alias for the distinct id')
+    .option('-a, --alias <previousId>', 'alias for the distinct id')
 
-  .parse(process.argv)
+    .parse(process.argv)
 
 if (program.args.length !== 0) {
-  program.help()
+    program.help()
 }
 
 const apiKey = program.apiKey
@@ -37,36 +37,36 @@ const properties = program.properties
 const alias = program.alias
 
 const run = (method, args) => {
-  const posthog = new PostHog(apiKey, { host, flushAt: 1 })
-  posthog[method](args, err => {
-    if (err) {
-      console.error(err.stack)
-      process.exit(1)
-    }
-  })
+    const posthog = new PostHog(apiKey, { host, flushAt: 1 })
+    posthog[method](args, (err) => {
+        if (err) {
+            console.error(err.stack)
+            process.exit(1)
+        }
+    })
 }
 
 switch (type) {
-  case 'capture':
-    run('capture', {
-      event,
-      properties,
-      distinctId
-    })
-    break
-  case 'identify':
-    run('identify', {
-      properties,
-      distinctId
-    })
-    break
-  case 'alias':
-    run('alias', {
-      alias,
-      distinctId
-    })
-    break
-  default:
-    console.error('invalid type:', type)
-    process.exit(1)
+    case 'capture':
+        run('capture', {
+            event,
+            properties,
+            distinctId,
+        })
+        break
+    case 'identify':
+        run('identify', {
+            properties,
+            distinctId,
+        })
+        break
+    case 'alias':
+        run('alias', {
+            alias,
+            distinctId,
+        })
+        break
+    default:
+        console.error('invalid type:', type)
+        process.exit(1)
 }
--- index.d.ts
+++ index.d.ts
@@ -1,64 +1,61 @@
 // Type definitions for posthog-node
 // Project: Posthog
 
-declare module "posthog-node" {
-  interface Option {
-    flushAt?: number;
-    flushInterval?: number;
-    host?: string;
-    api_host?: string;
-    enable?: boolean;
-  }
+declare module 'posthog-node' {
+    interface Option {
+        flushAt?: number
+        flushInterval?: number
+        host?: string
+        api_host?: string
+        enable?: boolean
+    }
 
-  interface CommonParamsInterfacePropertiesProp {
-    [key: string]:
-      | string
-      | number
-      | Array<any | { [key: string]: string | number }>;
-  }
+    interface CommonParamsInterfacePropertiesProp {
+        [key: string]: string | number | Array<any | { [key: string]: string | number }>
+    }
 
-  interface IdentifyMessage {
-    distinctId: string;
-    properties?: CommonParamsInterfacePropertiesProp;
-  }
+    interface IdentifyMessage {
+        distinctId: string
+        properties?: CommonParamsInterfacePropertiesProp
+    }
 
-  interface EventMessage extends IdentifyMessage {
-    event: string;
-  }
+    interface EventMessage extends IdentifyMessage {
+        event: string
+    }
 
-  export default class PostHog {
-    constructor(apiKey: string, options?: Option);
-    /**
-     * @description Capture allows you to capture anything a user does within your system,
-     * which you can later use in PostHog to find patterns in usage,
-     * work out which features to improve or where people are giving up.
-     * A capture call requires:
-     * @param distinctId which uniquely identifies your user
-     * @param event We recommend using [verb] [noun], like movie played or movie updated to easily identify what your events mean later on.
-     * @param properties OPTIONAL | which can be a dict with any information you'd like to add
-     */
-    capture({ distinctId, event, properties }: EventMessage): PostHog;
+    export default class PostHog {
+        constructor(apiKey: string, options?: Option)
+        /**
+         * @description Capture allows you to capture anything a user does within your system,
+         * which you can later use in PostHog to find patterns in usage,
+         * work out which features to improve or where people are giving up.
+         * A capture call requires:
+         * @param distinctId which uniquely identifies your user
+         * @param event We recommend using [verb] [noun], like movie played or movie updated to easily identify what your events mean later on.
+         * @param properties OPTIONAL | which can be a dict with any information you'd like to add
+         */
+        capture({ distinctId, event, properties }: EventMessage): PostHog
 
-    /**
-     * @description Identify lets you add metadata on your users so you can more easily identify who they are in PostHog,
-     * and even do things like segment users by these properties.
-     * An identify call requires:
-     * @param distinctId which uniquely identifies your user
-     * @param properties with a dict with any key: value pairs
-     */
-    identify({ distinctId, properties }: IdentifyMessage): PostHog;
+        /**
+         * @description Identify lets you add metadata on your users so you can more easily identify who they are in PostHog,
+         * and even do things like segment users by these properties.
+         * An identify call requires:
+         * @param distinctId which uniquely identifies your user
+         * @param properties with a dict with any key: value pairs
+         */
+        identify({ distinctId, properties }: IdentifyMessage): PostHog
 
-    /**
-     * @description To marry up whatever a user does before they sign up or log in with what they do after you need to make an alias call.
-     * This will allow you to answer questions like "Which marketing channels leads to users churning after a month?"
-     * or "What do users do on our website before signing up?"
-     * In a purely back-end implementation, this means whenever an anonymous user does something, you'll want to send a session ID with the capture call.
-     * Then, when that users signs up, you want to do an alias call with the session ID and the newly created user ID.
-     * The same concept applies for when a user logs in. If you're using PostHog in the front-end and back-end,
-     *  doing the identify call in the frontend will be enough.:
-     * @param distinctId the current unique id
-     * @param alias the unique ID of the user before
-     */
-    alias(data: { distinctId: string; alias: string }): PostHog;
-  }
+        /**
+         * @description To marry up whatever a user does before they sign up or log in with what they do after you need to make an alias call.
+         * This will allow you to answer questions like "Which marketing channels leads to users churning after a month?"
+         * or "What do users do on our website before signing up?"
+         * In a purely back-end implementation, this means whenever an anonymous user does something, you'll want to send a session ID with the capture call.
+         * Then, when that users signs up, you want to do an alias call with the session ID and the newly created user ID.
+         * The same concept applies for when a user logs in. If you're using PostHog in the front-end and back-end,
+         *  doing the identify call in the frontend will be enough.:
+         * @param distinctId the current unique id
+         * @param alias the unique ID of the user before
+         */
+        alias(data: { distinctId: string; alias: string }): PostHog
+    }
 }
